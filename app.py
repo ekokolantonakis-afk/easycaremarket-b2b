16700fd38e0b1710fbd69a86cde934345bde3cb5
ekokolantonakis-afk <e.kokolantonakis@gmail.com>
2025-08-28 12:24:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -214,5 +214,5 @@ def add_sample_data():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
